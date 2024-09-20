2f20ab845fcd90863131f5aafc67c8b3976506fa
tiffanyl220 <tiffanyl220@nycstudents.net>
2024-09-20 00:57:14 +0000

    9 and 10

---
+++ Feeder.java
@@ -0,0 +1,4 @@
+public class Feeder {
+
+
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Feeder f = new Feeder();
+        System.out.println(f);
+        System.out.println(currentFood);
+
+    }
+
+}
